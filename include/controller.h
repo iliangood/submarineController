165a5aa3319f2960aece899f17bc6221b210980f
iliangood <ilia191168@gmail.com>
2025-11-07 17:42:33 +0300

    работа на Controller

--- include/controller.h
+++ include/controller.h
@@ -3,13 +3,40 @@
 
 #include <inttypes.h>
 
+#include <SDL2/SDL.h>
+
 #include "axis.h"
+class Joystick
+{
+	SDL_GameController* joystick_;
+	Axises axises_;
+public:
+	SDL_GameController*& joystick()
+	{
+		return joystick_;
+	}
+
+};
+
 
 class Controller
 {
-	Axises axises_;
-public:
+	Axises mainAxises_;
+	
 	Controller();
+	Controller(const Controller&) = delete;
+	Controller& operator=(const Controller&) = delete;
+	void handleEvent(SDL_ControllerAxisEvent event);
+	void handleEvent(SDL_ControllerButtonEvent);
+
+public:
+	static Controller& getInstance()
+	{
+		static Controller controller;
+		return controller;
+	}
+
+	
 
 };
 
--- src/controller.cpp
+++ src/controller.cpp
@@ -1,1 +1,22 @@
 #include "controller.h"
+
+#include <iostream>
+
+#include <SDL2/SDL.h>
+
+
+Controller::Controller()
+{
+	if(SDL_Init(SDL_INIT_JOYSTICK | SDL_INIT_GAMECONTROLLER) < 0)
+	{
+		std::cerr << "SDL init failed: " << SDL_GetError();
+		return;
+	}
+	if(SDL_GameControllerAddMappingsFromFile("gamecontrollerdb.txt") < 0)
+	{
+		std::cerr << "SDL add mapping failed: " << SDL_GetError();
+		return;
+	}
+	int numJoysticks = SDL_NumJoysticks();
+	for()
+}
