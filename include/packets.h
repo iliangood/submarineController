af9be8185f702ea276944cd7298d20763fa6898f
iliangood <ilia191168@gmail.com>
2025-11-21 20:26:45 +0300

    Работа над передаяей данных

--- include/packets.h
+++ include/packets.h
@@ -4,12 +4,40 @@
 #include <inttypes.h>
 
 #include "axis.h"
+#include <array>
 
 struct ControllerPacket
 {
 	uint64_t sendTime_ms;
 	uint64_t last_packet_rx_time_message;
 	Axises speedTarget;
+
+	std::array<uint8_t, 28> serialize()
+	{
+		std::array<uint8_t, 28> data{};
+		*reinterpret_cast<uint64_t*>(&data[0]) = sendTime_ms;
+		*reinterpret_cast<uint64_t*>(&data[8]) = last_packet_rx_time_message;
+		for(size_t i = 0; i < 6; ++i)
+		{
+			*reinterpret_cast<int16_t*>(&data[16 + i * 2]) = speedTarget[i];
+		}
+		return data;
+	}
+	static ControllerPacket deserialize(const std::array<uint8_t, 28>& data)
+	{
+		ControllerPacket packet;
+		packet.sendTime_ms = *reinterpret_cast<const uint64_t*>(&data[0]);
+		packet.last_packet_rx_time_message = *reinterpret_cast<const uint64_t*>(&data[8]);
+		for(size_t i = 0; i < 6; ++i)
+		{
+			packet.speedTarget[i] = *reinterpret_cast<const int16_t*>(&data[16 + i * 2]);
+		}
+		return packet;
+	}
+	static constexpr size_t serializedSize()
+	{
+		return 28;
+	}
 };
 
 struct SubmarinePacket
@@ -18,6 +46,35 @@ struct SubmarinePacket
 	uint64_t last_packet_rx_time_message;
 	Axises currentSpeed;
 	Axises currentPos;
+
+	std::array<uint8_t, 40> serialize()
+	{
+		std::array<uint8_t, 40> data{};
+		*reinterpret_cast<uint64_t*>(&data[0]) = sendTime_ms;
+		*reinterpret_cast<uint64_t*>(&data[8]) = last_packet_rx_time_message;
+		for(size_t i = 0; i < 6; ++i)
+		{
+			*reinterpret_cast<int16_t*>(&data[16 + i * 2]) = currentSpeed[i];
+			*reinterpret_cast<int16_t*>(&data[28 + i * 2]) = currentPos[i];
+		}
+		return data;
+	}
+	static SubmarinePacket deserialize(const std::array<uint8_t, 40>& data)
+	{
+		SubmarinePacket packet;
+		packet.sendTime_ms = *reinterpret_cast<const uint64_t*>(&data[0]);
+		packet.last_packet_rx_time_message = *reinterpret_cast<const uint64_t*>(&data[8]);
+		for(size_t i = 0; i < 6; ++i)
+		{
+			packet.currentSpeed[i] = *reinterpret_cast<const int16_t*>(&data[16 + i * 2]);
+			packet.currentPos[i] = *reinterpret_cast<const int16_t*>(&data[28 + i * 2]);
+		}
+		return packet;
+	}
+	static constexpr size_t serializedSize()
+	{
+		return 40;
+	}
 };
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ int main()
 {
     debug_init();
 
-	spdlog::set_level(spdlog::level::debug);
+	spdlog::set_level(spdlog::level::trace);
 
     Controller::getInstance();
 	UDPTransmitter transmitter(56728, "submarine");
@@ -31,9 +31,14 @@ int main()
 		ReceiveInfo rci = transmitter.receiveData(&msg);
 		if(recieved(rci))
 		{
+			if(rci.dataSize != SubmarinePacket::serializedSize())
+			{
+				spdlog::warn("incorrect packet size from {}", rci.remoteIP.value_or(IPAddress(0,0,0,0)).toString());
+			}
 			SubmarinePacket inPacket = msg.read<SubmarinePacket>();
 			last_packet_rx_time_message = inPacket.sendTime_ms;
 			ping_ms = millis() - inPacket.last_packet_rx_time_message;
+			spdlog::info("received packet from {}", rci.remoteIP.value_or(IPAddress(0,0,0,0)).toString());
 		}
 		msg.clear();
 		if(std::chrono::steady_clock::now() - lastSend > std::chrono::milliseconds(10))
@@ -43,13 +48,14 @@ int main()
 					static_cast<uint64_t>(millis()),
 					last_packet_rx_time_message,
 					Controller::getInstance().axises()
-				});
+				}.serialize());
+			transmitter.sendData(msg);
 		}
 		msg.clear();
 		const Axises& axises = Controller::getInstance().axises();
 		std::cout << axises[0] << ' ' << axises[1] << ' ' << axises[2] << ' ' << axises[3] << ' ' << axises[4] << ' ' << axises[5] << '\n';
 		std::cout << "ping:" << ping_ms << "ms" << std::endl;
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
 	}
 	
     return 0;
