17e117576019314bbe0a78fd42411f6ddc1b3e01
iliangood <ilia191168@gmail.com>
2026-01-08 20:34:51 +0300

    Убрал лишнюю отладку

--- src/main.cpp
+++ src/main.cpp
@@ -26,19 +26,31 @@ int main()
 	std::chrono::steady_clock::time_point lastSend = std::chrono::steady_clock::now();
 	uint64_t last_packet_rx_time_message = 0;
 	uint64_t ping_ms = 0;
+
+	SDL_Event event;
+
 	while(true)
 	{
+
+		while (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT) {
+            return 0; // Завершить программу при Ctrl+C или закрытии окна
+        }
+    }
+
 		ReceiveInfo rci = transmitter.receiveData(&msg);
 		if(recieved(rci))
 		{
-			if(rci.dataSize != SubmarinePacket::serializedSize())
+			/*if(rci.dataSize != SubmarinePacket::serializedSize())
 			{
 				spdlog::warn("incorrect packet size from {}", rci.remoteIP.value_or(IPAddress(0,0,0,0)).toString());
-			}
+			}*/
 			SubmarinePacket inPacket = msg.read<SubmarinePacket>();
 			last_packet_rx_time_message = inPacket.sendTime_ms;
 			ping_ms = millis() - inPacket.last_packet_rx_time_message;
 			spdlog::info("received packet from {}", rci.remoteIP.value_or(IPAddress(0,0,0,0)).toString());
+			const Axises& Saxises = inPacket.currentPos;
+			std::cout << Saxises[3] << ' ' << Saxises[4] << ' ' << Saxises[5] << std::endl;
 		}
 		msg.clear();
 		if(std::chrono::steady_clock::now() - lastSend > std::chrono::milliseconds(100))
@@ -50,13 +62,13 @@ int main()
 					Controller::getInstance().axises()
 				}.serialize());
 			transmitter.sendData(msg);
-			std::cout << "time:" << millis() << '\n';
+			//std::cout << "send time:" << millis() << '\n';
 		}
 		msg.clear();
 		const Axises& axises = Controller::getInstance().axises();
-		std::cout << axises[0] << ' ' << axises[1] << ' ' << axises[2] << ' ' << axises[3] << ' ' << axises[4] << ' ' << axises[5] << '\n';
-		std::cout << "ping:" << ping_ms << "ms" << std::endl;
-		std::cout << "target IP:" << transmitter.targetIP() << std::endl;
+		//std::cout << axises[0] << ' ' << axises[1] << ' ' << axises[2] << ' ' << axises[3] << ' ' << axises[4] << ' ' << axises[5] << '\n';
+		//std::cout << "ping:" << ping_ms << "ms" << std::endl;
+		//std::cout << "target IP:" << transmitter.targetIP() << std::endl;
 		std::this_thread::sleep_for(std::chrono::milliseconds(10));
 	}
 	
